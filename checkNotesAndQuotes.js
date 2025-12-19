139a4a1ef4a0f4f3c8a54065d348e3e4fd68cd94
Samson <93052891+SamB-C@users.noreply.github.com>
2025-12-19 19:48:28 +0000

    Comment converting poems
    Add comments to checkNotesAndQuotes.js and convertPoems.js

--- checkNotesAndQuotes.js
+++ checkNotesAndQuotes.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const FAKE_SPACE = '|+|'
 
+/**
+ * 
+ * @param {object} prevNotes The previous notes for the poem.
+ * @param {string[][]} prevQuotes A list of quotes.
+ * @param {string} newContent New poem content.
+ * @returns All the notes and quotes that are valid given the new content.
+ */
 function checkNotesAndQuotes(prevNotes, prevQuotes, newContent) {
+    // Find which notes and quotes are valid and invalid.
     const notesResult = checkNotes(prevNotes, newContent);
     const quotesResult = checkQuotes(prevQuotes, newContent);
+    // Add invalid notes and quotes to './invalidNotesAndQuotes.json' so they can be fixed.
     const invalidNotesAndQuotesJSON = fs.readFileSync('./invalidNotesAndQuotes.json');
     const invalidNotesAndQuotes = JSON.parse(invalidNotesAndQuotesJSON);
     Object.keys(notesResult["invalid"]["word-no-longer-exists"]).forEach(invalidNote => {
@@ -31,11 +40,18 @@ function checkNotesAndQuotes(prevNotes, prevQuotes, newContent) {
     }
 }
 
+/**
+ * @param {object} prevNotes Object containing the previous notes.
+ * @param {string} newContent New poem string.
+ * @returns Object containing which notes are valid and invalid, and if invalid, why.
+ */
 function checkNotes(prevNotes, newContent) {
     const validNotes = {}
     const invalidNotes = {}
     if (prevNotes !== undefined) {
+        // Iterate over previous notes.
         Object.keys(prevNotes).forEach(noteText => {
+            // Check if each word still exists.
             let valid = true;
             prevNotes[noteText].forEach(wordSection => {
                 valid = checkWordSectionExists(wordSection, newContent)
@@ -47,6 +63,7 @@ function checkNotes(prevNotes, newContent) {
             }
         })
     }
+    // Check
     const { overlappingNotes, nonOverlappingNotes } = checkOverlaps(validNotes)
     return {
         "invalid": {
@@ -57,12 +74,20 @@ function checkNotes(prevNotes, newContent) {
     }
 }
 
+/**
+ * Checks if quotes are valid, and if not, indicates why.
+ * @param {string[][]} prevQuotes 
+ * @param {string} newContent 
+ * @returns object indicating whether quote is invalid or valid, and if invalid, why it is invalid.
+ */
 function checkQuotes(prevQuotes, newContent) {
     const validQuotes = [];
     const invalidQuotes = [];
     if (prevQuotes !== undefined) {
+        // Iterate over all quotes.
         prevQuotes.forEach(quoteToCheck => {
             let valid = true;
+            // Check if each word still exists
             quoteToCheck.forEach(wordSection => {
                 valid = checkWordSectionExists(wordSection, newContent);
             });
@@ -73,6 +98,7 @@ function checkQuotes(prevQuotes, newContent) {
             }
         });
     }
+    // Check if words in quotes are consecutive, and if quotes overlap.
     const { consecutiveWordsQuotes, nonConsecutiveWordsQuotes } = checkQuotesConsecutive(validQuotes, newContent);
     const { nonOverlappingQuotes, overlappingQuotes } = checkQuoteOverlaps(consecutiveWordsQuotes);
     return {
@@ -85,23 +111,34 @@ function checkQuotes(prevQuotes, newContent) {
     };
 }
 
+/**
+ * Finds notes that have a word section appearing in more than three notes.
+ * @param {object} prevNotes Object containing the previous notes.
+ * @returns Object partitioning notes into those that contain overlaps, and those that don't.
+ */
 function checkOverlaps(prevNotes) {
     const returnValue = {
         overlappingNotes: {},
         nonOverlappingNotes: {},
     };
+    // Iterate over each note.
     Object.keys(prevNotes).forEach(noteText => {
         valid = true;
+        // Iterate over highlighted words in note.
         prevNotes[noteText].forEach(wordSection => {
             let numberOfQuotesWordSectionIsIn = 1;
+            // Iterate over all notes.
             Object.keys(prevNotes).forEach(noteTextForNoteToCheckAgainst => {
+                // Get words in quote to check against
                 const noteToCheckAgainstContent = prevNotes[noteTextForNoteToCheckAgainst];
+                // Increment counter of how many times word appears in other notes
                 if (noteText !== noteTextForNoteToCheckAgainst) {
                     if (noteToCheckAgainstContent.includes(wordSection)) {
                         numberOfQuotesWordSectionIsIn++;
                     }
                 }
             });
+            // If word is in more than 3 notes, mark as invalid.
             if (numberOfQuotesWordSectionIsIn > 3) {
                 valid = false;
             }
@@ -115,11 +152,17 @@ function checkOverlaps(prevNotes) {
     return returnValue;
 }
 
+/**
+ * Finds which quotes overlap, and which don't
+ * @param {string[][]} quotes 
+ * @returns Object containing which quotes don't overlap, and which do.
+ */
 function checkQuoteOverlaps(quotes) {
     const returnValue = {
         overlappingQuotes: [],
         nonOverlappingQuotes: []
     };
+    // Check if each word in a quote is not in another quote.
     quotes.forEach(quote => {
         let valid = true;
         quote.forEach(word => {
@@ -138,16 +181,29 @@ function checkQuoteOverlaps(quotes) {
     return returnValue;
 }
 
+/**
+ * Removes any numbers from a word.
+ * @param {string} word 
+ * @returns {string}
+ */
 function removeNumbers(word) {
     return word.split('').filter(letter => !letter.match(/[0-9]/)).join('');
 }
 
+/**
+ * Gets all the word sections in the poem, in order.
+ * @param {string} poemContent 
+ * @returns {string[][]}
+ */
 function getAllWordsInPoem(poemContent) {
+    // Get lines
     const lines = poemContent.split('\n');
     const words = [];
+    // Get words
     lines.forEach(line => {
         const wordsInLine = line.split(' ');
         const wordSections = [];
+        // Get word sections
         wordsInLine.forEach(word => {
             const wordSectionsOfSingleWord = word.split(FAKE_SPACE);
             wordSections.push(...wordSectionsOfSingleWord);
@@ -157,16 +213,24 @@ function getAllWordsInPoem(poemContent) {
     return words
 }
 
+/**
+ * Checks if all the words in a quote are consecutive in the poem.
+ * @param {string[][]} quotes 
+ * @param {string} poemContent 
+ * @returns Object of which quotes are consecutive or not.
+ */
 function checkQuotesConsecutive(quotes, poemContent) {
     const returnValue = {
         consecutiveWordsQuotes: [],
         nonConsecutiveWordsQuotes: []
     }
+    // Get all words in the poem
     const allWordsinPoem = getAllWordsInPoem(poemContent);
     quotes.forEach(quote => {
         const wordsNoSpaces = allWordsinPoem.filter(word => removeNumbers(word) !== '')
         const indexOfFirstWord = wordsNoSpaces.indexOf(quote[0])
         let valid = true;
+        // Check that the words are consecutive.
         quote.forEach((word, index) => {
             const nextWordInPoem = wordsNoSpaces[indexOfFirstWord + index];
             if (word !== nextWordInPoem) {
@@ -182,6 +246,12 @@ function checkQuotesConsecutive(quotes, poemContent) {
     return returnValue;
 }
 
+/**
+ * Checks that a word section actually exists in the new poem
+ * @param {string} wordSection word to check 
+ * @param {string} newContent poem content to check against
+ * @returns {bool} Whether wordSection exists in poem
+ */
 function checkWordSectionExists(wordSection, newContent) {
     return newContent.includes(wordSection);
 }
--- convertPoems.js
+++ convertPoems.js
@@ -7,26 +7,34 @@ const FAKE_SPACE = '|+|';
 fs.readFile('./rawPoems.json', 'utf8', (err, data) => {
     if (err) throw err;
 
+    // Gets existing Converted Poems
     const prevConvertedPoemsJSON = fs.readFileSync('./convertedPoems.json', {encoding: 'utf-8'});
     const prevConvertedPoems = JSON.parse(prevConvertedPoemsJSON);
 
     const result = {};
     const poems = JSON.parse(data);
     const poemSettings = getPoemSettings();
+    // Iterate over all poems
     Object.entries(poems).map((keyValuePair) => {
         const poemName = keyValuePair[0];
         const poemFileContent = keyValuePair[1];
 
         const { poemContent, poemAuthor } = getPoemNameContentAuthor(poemFileContent);
 
+        // Adds instance numbers around words, and adds in fake spaces where necessary.
         const poemInfo = addInstanceNumbersToWords(poemName, poemContent);
 
+        // Create object to write.
         result[poemName] = poemInfo;
         result[poemName]["author"] = poemAuthor;
         if (prevConvertedPoems[poemName] !== undefined) {
+            // Poem already existed, so copy over notes and quotes. (Although poem may have been altered)
+            // TODO add seperate check before poemInfo, to see if poem hasn't changed at all
             const prevQuotes = prevConvertedPoems[poemName]["quotes"];
             const prevNotes = prevConvertedPoems[poemName]["notes"];
             const newPoemContent = result[poemName]['convertedPoem'];
+            // Check that no notes and quotes have been made invalid by the edit
+            // TODO, automatically shift indexes in notes and quotes as needed if poem altered
             const {validNotes, validQuotes} = checkNotesAndQuotes(prevNotes, prevQuotes, newPoemContent);
             result[poemName]['quotes'] = validQuotes
             result[poemName]['notes'] = validNotes;
@@ -40,6 +48,11 @@ fs.readFile('./rawPoems.json', 'utf8', (err, data) => {
     fs.writeFile('./convertedPoems.json', JSON.stringify(result, null, 4), (err) => {if (err) {throw err;} else {console.log('\nAll poems complete!')}});
 });
 
+/**
+ * Gets the poem name, content, and author from the raw string.
+ * @param {String} poemFileContent Content of poem as string.
+ * @returns {poemName: string, poemContent: string, poemAuthor: string}
+ */
 function getPoemNameContentAuthor(poemFileContent) {
     const listOfLines = poemFileContent.split('\n');
     const poemName = listOfLines.splice(0, 2)[0];
@@ -52,12 +65,22 @@ function getPoemNameContentAuthor(poemFileContent) {
     }
 }
 
+/**
+ * @returns An object contaning the poem settings stored in './poems/poemSettings.json'.
+ */
 function getPoemSettings() {
     const poemSettingsJSON = fs.readFileSync('./poems/poemSettings.json', {encoding: 'utf8'});
     const poemSettings = JSON.parse(poemSettingsJSON)
     return poemSettings
 }
 
+/**
+ * Copy poem settings for this poem into result. Poem settings are currently only if poem is centered or not.
+ * This is where I should also add the anthology it is in, and position in the anthology.                           <----- IMPORTANT
+ * @param {object} poemSettings The poem settings stored in './poems/poemSettings.json'.
+ * @param {string} poemName
+ * @param {object} result Object to write settings to.
+ */
 function addSettings(poemSettings, poemName, result) {
     const centeredPoems = poemSettings["centered"];
     if (centeredPoems.includes(poemName)) {
@@ -67,7 +90,12 @@ function addSettings(poemSettings, poemName, result) {
     }
 }
 
-// Gets add the instance numbers to the words in the poem, and gets the poem's word count
+/**
+ * Gets add the instance numbers to the words in the poem, and gets the poem's word count
+ * @param {string} poemName
+ * @param {string} poemContent 
+ * @returns {convertedPoem: , wordCount: int}
+ */
 function addInstanceNumbersToWords(poemName, poemContent) {
     const wordsAlreadyInPoem = {"__words__": 0};
     return {
@@ -77,7 +105,12 @@ function addInstanceNumbersToWords(poemName, poemContent) {
 }
 
 
-// Maps over all the lines in the poem
+/**
+ * Maps over all the lines in the poem.
+ * @param {string} poem 
+ * @param {object} wordsAlreadyInPoem Object containing a count of existing words in the poem
+ * @returns String of poem with word indexes wrapped around each word.
+ */
 function mapOverLines(poem, wordsAlreadyInPoem) {
     const returnValue =  poem.split(/\n/)
                 .map((line) => lineMapFunction(line, wordsAlreadyInPoem))
@@ -85,7 +118,13 @@ function mapOverLines(poem, wordsAlreadyInPoem) {
     return returnValue
 }
 
-// Maps over all the words in the poem
+// 
+/**
+ * Maps over all the words in each line. Also increments the word count.
+ * @param {string} line 
+ * @param {object} wordsAlreadyInPoem Object containing a count of existing words in the poem
+ * @returns String of line with word indexes wrapped around each word.
+ */
 function lineMapFunction(line, wordsAlreadyInPoem) {
     const returnValue = line.split(' ')
                 .map((word) => {
@@ -96,7 +135,11 @@ function lineMapFunction(line, wordsAlreadyInPoem) {
     return returnValue;
 }
 
-// Increases the word count (if not space)
+/**
+ * Increases the word count if the word is not a space.
+ * @param {string} word 
+ * @param {object} wordsAlreadyInPoem Object containing a count of existing words in the poem
+ */
 function increaseWordCount(word, wordsAlreadyInPoem) {
     // Only increases word count for words that are not a space
     if (word) {
@@ -104,7 +147,12 @@ function increaseWordCount(word, wordsAlreadyInPoem) {
     }
 }
 
-// Sorts out whether the word has a special character and deals with it, before giving the word instance numbers
+/**
+ * Sorts out whether the word has a special character and deals with it, before giving the word instance numbers
+ * @param {string} word 
+ * @param {object} wordsAlreadyInPoem Object containing a count of existing words in the poem
+ * @returns String with word wrapped in instance numbers.
+ */
 function wordMapFunction(word, wordsAlreadyInPoem) {
     if (word.match(SPECIAL_CHARACTER_REGEX)) {
         // Add the fake space between text and special character
@@ -120,7 +168,11 @@ function wordMapFunction(word, wordsAlreadyInPoem) {
     }
 }
 
-// Add the fake space between text and special character
+/**
+ * Add the fake space between text and special character.
+ * @param {string} word word containing a special character.
+ * @returns String with word and special character seperated by fake space |+|.
+ */
 function specialCharacterCaseGetWordSections(word) {
     return word.split('')
     .map((letter) => {
@@ -131,7 +183,12 @@ function specialCharacterCaseGetWordSections(word) {
     }).join('');
 }
 
-// Adds instance numbers to word sections
+/**
+ * Adds instance numbers to word sections
+ * @param {string} word Word to add instance numbers to
+ * @param {object} instances Object containing a count of existing words in the poem
+ * @returns Word wrapped in instance numbers.
+ */
 function giveWordInstanceNumber(word, instances) {
     if (word in instances) {
         instances[word] = instances[word] + 1;
